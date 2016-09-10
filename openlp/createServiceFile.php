1b211e90b46e9f71ca1eae17b061af794f643066
b-dur <b-dur@b-dur.dk>
2016-09-10 17:41:53 +0200

    Moved finding songid based on eventid into the serviceCreator class

--- openlp/createServiceFile.php
+++ openlp/createServiceFile.php
@@ -17,20 +17,7 @@ $content = new ServiceCreator();
 $eventId = $_GET['eventId'];
 // Should we do the whole database, or only some chosen ones.
 if ($eventId) {
-	$content->insertCustom(" ", "", "  ");
-
-    $query = "SELECT SangID, Dato FROM ProgramPunkt INNER JOIN Program ON ProgramPunkt.ProgramID = Program.ProgramID WHERE Program.ProgramID = " . $eventId ." ORDER BY Raekkefoelge";
-    $result = doSQLQuery($query);
-    $line = db_fetch_array($result);
-    $content->serviceName = substr($line['Dato'], 0, 10) . ".osz";
-	do {
-        if ($content->insertSong($line['SangID'])==false) {
-			echo "ERROR when inserting $songid!";
-			break;
-		}
-    	$content->insertCustom(" ", "", "  ");
-    } while ($line = db_fetch_array($result));
-
+    $content->createFromEventId($eventId);
 } else {
 	$content->insertAllSongs();
 }
